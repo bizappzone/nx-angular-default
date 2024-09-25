87b73f2007161fba2bad3203916a940967fe7963
AJ Munro <arthur.munro@tbs-sct.gc.ca>
2024-09-25 12:16:04 -0400

    create store

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Route } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component'; // Adjust the import path as necessary
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{ path: '', component: HomePageComponent }];
